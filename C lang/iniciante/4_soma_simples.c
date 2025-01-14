5dde79eb5c56450783b1542fbf53c57d4b0b65b3
Pedro <peecampossobrinho@gmail.com>
2025-01-14 19:38:33 -0400

    4

---
+++ C lang/iniciante/4_soma_simples.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int A, B;
+    int soma;
+    
+    scanf("%d %d",&A,&B);
+    
+    soma = A + B;
+    
+    printf("SOMA = %d\n", soma);
+    
+    return 0;
+}
